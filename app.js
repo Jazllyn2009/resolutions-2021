ea43abf3084007b036256fea3a323ab54ea2dc7c
Jazllyn2009 <70760446+Jazllyn2009@users.noreply.github.com>
2021-01-05 18:34:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+//method 1
+var family;
+family=document.getElementById("family")
+
+//change color
+family.style.color("white")
+
+//method 2 
+family.innerHTML-"J Family"
+
+//how to print text
+var john= "Hello My name is John";
+document.write(john);
+
+//for reisning data the document.write(john)should be below the line which should be written
+var john="Hello Im john I will play in the evenings"
+document.write(john); 
+
+//arrays
+//method 1
+var SolarSystem=["Earth", "Venus", "Neptune", "Mercury"]
+//method 2
+var SolarSystem=[
+"Earth",
+ "Venus",
+ "Neptune",
+ "Mercury"];
+
+ //method 3
+ var solarSystem= new Array ("Earth", "Venus", "Neptune", "Mercury")
